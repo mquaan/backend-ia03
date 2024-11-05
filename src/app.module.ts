e580b904be85b1265c55c41c52cb76d1ee4430cc
mquaan <nmquan.se@gmail.com>
2024-11-05 20:48:33 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
 	controllers: [AppController],
+	providers: [AppService],
 	imports: [
 		ConfigModule.forRoot(), // Load .env file
 		MongooseModule.forRoot(process.env.DATABASE_URI), // Use the DATABASE_URI from .env
